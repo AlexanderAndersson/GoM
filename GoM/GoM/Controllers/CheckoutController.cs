364e588e5bcffb242f3ce4394cb28166eaa49026
callesandstrom <callesandstrom@hotmail.com>
2016-04-06 20:45:46 +0200

    Fixade lite med kundvagnen

---
+++ GoM/GoM/Controllers/CheckoutController.cs
@@ -0,0 +1,18 @@
+﻿using GoM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoM.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return View(Database.Account.ShoppingCart.Products);
+        }
+    }
+}
