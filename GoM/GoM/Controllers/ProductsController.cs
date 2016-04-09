705b499cae42724e50f37c3b829f777a273c732c
mcr8802 <teh.muzzi@gmail.com>
2016-04-09 18:33:45 +0200

    Commiting because I removed a space and I hope this will fix this merge problem or I am going to break someones lovely neck.

--- GoM/GoM/Controllers/ProductsController.cs
+++ GoM/GoM/Controllers/ProductsController.cs
@@ -6,48 +6,48 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace GoM.Controllers
-{
-    public class ProductsController : Controller
     {
+    public class ProductsController : Controller
+        {
         // GET: Products
         public ActionResult Index()
-        {
+            {
             return View(Database.Albums);
-        }
+            }
 
         public ActionResult AddToCart(int id, string returnURL)
-        {
+            {
             //Hämtar id:t på albumet och hämtar sedan ett album från "databasen" utifrån det.
             //int id = Convert.ToInt32(Request.Form.Get("id"));
-            var album = Database.Albums.Where(a => a.Id == id).First();
+            var album = Database.Albums.Where(a => a.Id==id).First();
 
             //Om albumet har behållning...
-            if (album.InStock > 0)
-            {
-                //och om det inte finns en inloggad användare...
-                if (Database.Account == null)
+            if(album.InStock>0)
                 {
+                //och om det inte finns en inloggad användare...
+                if(Database.Account==null)
+                    {
                     //så skickar vi användaren till logga in/registrera-sidan
                     return RedirectToAction("LogInRegister", "Account");
-                }
+                    }
 
                 //Om användaren inte redan har albumet i sin varukorg...
-                if (!Database.Account.ShoppingCart.Products.Any(p => p.Album == album))
-                {
+                if(!Database.Account.ShoppingCart.Products.Any(p => p.Album==album))
+                    {
                     //så skapar vi en ny produkt med albumet och lägger i varukorgen.
-                    Database.Account.ShoppingCart.Products.Add(new Product { Album = album, Quantity = 1 });
-                }
+                    Database.Account.ShoppingCart.Products.Add(new Product { Album=album, Quantity=1 });
+                    }
 
                 //Annars om kvantiteten användaren har av produkten är mindre än behållningen av albumet.
-                else if (Database.Account.ShoppingCart.Products.Where(p => p.Album == album).First().Quantity < album.InStock)
-                {
+                else if(Database.Account.ShoppingCart.Products.Where(p => p.Album==album).First().Quantity<album.InStock)
+                    {
                     //så ökar vi kvantiteten med 1...
-                    Database.Account.ShoppingCart.Products.Where(p => p.Album == album).First().Quantity++;
+                    Database.Account.ShoppingCart.Products.Where(p => p.Album==album).First().Quantity++;
+                    }
                 }
-            }
             //för att få Searchfunktionen att fungera. Den ger tillbaka samma result även när man addat, annars går den till index.
             return Redirect(returnURL);
             //return RedirectToAction("Index");
             }
+        }
     }
-}
